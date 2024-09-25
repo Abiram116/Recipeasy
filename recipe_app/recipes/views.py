9ad3412ce4202ce85275ad03393a4a29367fce4d
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2024-09-25 19:46:18 +0530

    CSS Chnages

--- recipe_app/recipes/views.py
+++ recipe_app/recipes/views.py
@@ -7,11 +7,9 @@ from recipes.forms import RecipeForm
 from .models import Recipe, UserRecipe
 
 def home(request):
-    # Remove the filter to get all recipes
-    inspiring_recipes = Recipe.objects.all()
-    
+    inspiring_recipes = Recipe.objects.filter(is_inspiring=True)
     for recipe in inspiring_recipes:
-        recipe.ingredients_list = recipe.get_ingredients_list()
+        recipe.ingredients_list = [ingredient.strip() for ingredient in recipe.ingredients.replace('\n', ',').split(',') if ingredient.strip()]
     
     context = {
         'inspiring_recipes': inspiring_recipes,
@@ -60,50 +58,27 @@ def clear_my_recipes(request):
     UserRecipe.objects.filter(user=request.user).delete()
     return redirect('my_recipes')
 
-@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     ingredients = [ingredient.strip() for ingredient in recipe.ingredients.replace('\n', ',').split(',') if ingredient.strip()]
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Recipe
-
-def home(request):
-    inspiring_recipes = Recipe.objects.all()[:6]
-    for recipe in inspiring_recipes:
-        recipe.ingredients_list = recipe.ingredients.split(',')
-    context = {
-        'inspiring_recipes': inspiring_recipes,
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'recipe_detail.html', {'recipe': recipe, 'ingredients': ingredients})
 
 
-def add_to_my_recipes(request, recipe_id):
+def add_to_my_recipes_session(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     my_recipes = request.session.get('my_recipes', [])
     
     if recipe_id not in my_recipes:
         my_recipes.append(recipe_id)
-        # save it back to the session
     request.session['my_recipes'] = my_recipes
     return redirect('home')
 
-
-def my_recipes(request):
-    # get the recipe IDs stored in the session
+def my_recipes_session(request):
     recipe_ids = request.session.get('my_recipes', [])
-    
-    # Retrieve the actual Recipe objects from the database
     recipes = Recipe.objects.filter(id__in=recipe_ids)
-    
     return render(request, 'my_recipes.html', {'recipes': recipes})
 
-def clear_my_recipes(request):
+def clear_my_recipes_session(request):
     if 'my_recipes' in request.session:
         del request.session['my_recipes']
     return redirect('my_recipes')
-
-def recipe_detail(request, pk):
-    # Retrieve the recipe by primary key (pk)
-    recipe = get_object_or_404(Recipe, pk=pk)
-    ingredients = [ingredient.strip() for ingredient in recipe.ingredients.split(',')]
-    return render(request, 'recipe_detail.html', {'recipe': recipe, 'ingredients': ingredients})
