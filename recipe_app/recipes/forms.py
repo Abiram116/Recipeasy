68f605c6d15608f5d7f8bd0e833d5fc3bcf268fd
nandhithr6 <ys.nandhitha@gmail.com>
2024-11-13 15:47:29 +0530

    tags verification

--- recipe_app/recipes/forms.py
+++ recipe_app/recipes/forms.py
@@ -6,11 +6,10 @@ from .models import Recipe, RecipeSchedule, Tag
 class RecipeForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-        required=True,  # Set this to True to enforce selecting at least one tag
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
         label="Tags"
     )
-
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'image', 'rating', 'is_inspiring', 'ingredients', 'tags']
@@ -21,19 +20,7 @@ class RecipeForm(forms.ModelForm):
             'rating': forms.Select(attrs={'class': 'form-control'}, choices=[(i, str(i)) for i in range(1, 6)]),
             'is_inspiring': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'ingredients': forms.Textarea(attrs={'class': 'form-control recipe-textarea', 'rows': 5, 'placeholder': 'List your ingredients'}),
-            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'})  # Checkbox for tags
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Optionally, you can filter available tags based on some criteria, e.g., the current user
-        # self.fields['tags'].queryset = Tag.objects.filter(user=user)
-
-    def clean_tags(self):
-        tags = self.cleaned_data.get('tags')
-        if not tags:
-            raise ValidationError('At least one tag is required.')  # Custom error message if no tags are selected
-        return tags
 
 class RecipeScheduleForm(forms.ModelForm):
     class Meta:
